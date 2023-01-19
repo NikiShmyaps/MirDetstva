d94b89c4c1e3c88bee3de043932f25ba618a74f5
NNikitin <nnikitin@web3tech.ru>
2023-01-19 15:04:01 +0300

    change api

--- pages/api/FormSendingMail.js
+++ pages/api/FormSendingMail.js
@@ -14,20 +14,46 @@ export default async (req, res) => {
     },
   })
 
-  const mailData = {
+  const {
+    name = '',
+    phone = '',
+    adult = '',
+    children = '',
+    comment = ''
+  } = req.body
+
+  await transporter.sendMail({
     from: 'mirDetsva.contact@yandex.ru',
     to: 'mirdetstva.tver@yandex.ru',
-    subject: `${req.body.name} Оставил новую заявка`,
+    subject: `${name} оставил новую заявка`,
     html: `<div>
-      Пользователь ${req.body.name} оставил заявку, его номер телефона +7${req.body.phone}<br/>
-      Количество взрослых: ${req.body.adult}<br/>
-      Количество детей: ${req.body.children}<br/>
-      ${req.body.comment ? `Так же пользователь оставил комментарий: ${req.body.comment}` : ''}
+      Пользователь ${name} оставил заявку, его номер телефона +7${phone}<br/>
+      Количество взрослых: ${adult}<br/>
+      Количество детей: ${children}<br/>
+      ${comment ? `Так же пользователь оставил комментарий: ${comment}` : ''}
     </div>`
-  }
-  await transporter.sendMail(mailData, function (err, info) {
-    if(err) {console.log(err)}
-    else {console.log(info)}
   })
+
+  // res.json({message:
+  // `
+  //   Ethereal Mail Message ID: ${info.messageId}
+  //   Name from form: ${name}
+  // `})
+
+  // const mailData = {
+  //   from: 'mirDetsva.contact@yandex.ru',
+  //   to: 'mirdetstva.tver@yandex.ru',
+  //   subject: `${req.body.name} Оставил новую заявка`,
+  //   html: `<div>
+  //     Пользователь ${req.body.name} оставил заявку, его номер телефона +7${req.body.phone}<br/>
+  //     Количество взрослых: ${req.body.adult}<br/>
+  //     Количество детей: ${req.body.children}<br/>
+  //     ${req.body.comment ? `Так же пользователь оставил комментарий: ${req.body.comment}` : ''}
+  //   </div>`
+  // }
+  // await transporter.sendMail(mailData, function (err, info) {
+  //   if(err) {console.log(err)}
+  //   else {console.log(info)}
+  // })
   res.status(200).end()
 }
