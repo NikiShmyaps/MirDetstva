017f516bc97c3e6b5b8641455f51dc106715cb03
NikiShmyaps <mister.nikneim2013@yandex.ru>
2024-10-14 12:36:33 +0300

    добавил банер для НГ и новый фото локации

--- src/blocks/Main/index.tsx
+++ src/blocks/Main/index.tsx
@@ -1,13 +1,24 @@
 import { FC } from 'react';
+import styles from './main.module.scss';
 import { Description } from './Description';
 import { Gallery } from './Gallery';
 import { Package } from './Package';
+import {Image} from "@components";
 
 export const Main: FC = () => {
 
 	return (
 		<>
 			<Description/>
+			<div className={styles.snowPresent}>
+				<Image
+					wrapperClassName={styles.snowPresent_img}
+					src="./assets/snow.jpg"
+					width="680"
+					height="906"
+					alt="snow-present"
+				/>
+			</div>
 			<Gallery />
 			<Package />
 		</>
