1d567dcb833199e30dea656bc3b816c5bbdb7828
NikiShmyaps <mister.nikneim2013@yandex.ru>
2025-10-19 00:38:06 +0300

    new banners

--- src/blocks/Main/index.tsx
+++ src/blocks/Main/index.tsx
@@ -10,15 +10,22 @@ export const Main: FC = () => {
 	return (
 		<>
 			<Description/>
-			{/* <div className={styles.snowPresent}>
+			<div className={styles.snowPresent}>
+				<Image
+					wrapperClassName={styles.snowPresent_img}
+					src="./assets/masterClass.jpg"
+					width="550"
+					height="800"
+					alt="master-class"
+				/>
 				<Image
 					wrapperClassName={styles.snowPresent_img}
 					src="./assets/snow.jpg"
-					width="680"
-					height="906"
+					width="550"
+					height="800"
 					alt="snow-present"
 				/>
-			</div> */}
+			</div>
 			<Gallery />
 			<Package />
 		</>
